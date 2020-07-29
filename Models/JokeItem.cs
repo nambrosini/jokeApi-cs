4bef5f7ccdc9bb79adf50754e6b01df8686d723e
nambrosini <nambrosini@protonmail.com>
2020-07-29 16:32:51 +0200

    Added joke model

---
+++ Models/JokeItem.cs
@@ -0,0 +1,8 @@
+public class JokeItem
+{
+    public long Id { get; set; }
+    public string Content { get; set; }
+    public int Upvotes { get; set; }
+    public int Downvotes { get; set; }
+    public string Author { get; set; }
+}
